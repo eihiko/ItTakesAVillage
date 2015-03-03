46086312e1d978005977a3c87aca57b6ee3779e0
Julie Kaplan <jak266@case.edu>
2015-03-03 16:00:06 -0500

    Moved project to correct folder

---
+++ Assets/BuildingScripts/Building.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Building : MonoBehaviour {
+
+	public Vector2 size; //how many squares it takes up
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	//This can be called for a visual effect when there is a conflict
+	public void setConflict(bool isConflict) {
+		//Temporary code
+		/*if (isConflict) {
+				this.renderer.material;
+		} else {
+			this.renderer.material.color.b = 255;
+		}*/
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/BuildingScripts/BuildingSelector.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingSelector : MonoBehaviour {
+
+	public Building[] prefabs;
+	public BuildingPlacer wheretoPlace;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			Place (0);
+		}
+		else if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			Place (1);
+		}
+		else if (Input.GetKeyDown (KeyCode.Alpha3)) {
+			Place (2);
+		}
+		else if (Input.GetKeyDown (KeyCode.Alpha4)) {
+			Place (3);
+		}
+		else if (Input.GetKeyDown (KeyCode.Alpha5)) {
+			Place (4);
+		}
+		else if (Input.GetKeyDown (KeyCode.Alpha6)) {
+			Place (5);
+		}
+		else if (Input.GetKeyDown (KeyCode.Alpha7)) {
+			Place (6);
+		}
+		else if (Input.GetKeyDown (KeyCode.Alpha8)) {
+			Place (7);
+		}
+		else if (Input.GetKeyDown (KeyCode.Alpha9)) {
+			Place (8);
+		}
+		else if (Input.GetKeyDown (KeyCode.Alpha0)) {
+			Place (9);
+		}
+	
+	}
+
+	void Place (int objectNumber) {
+		if (prefabs.Length>objectNumber)
+		{
+			wheretoPlace.Place((Building)Instantiate(prefabs[objectNumber]));
+		}
+	}
+}
