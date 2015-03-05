fc380f4f0b8ec52ef3d72e1c3dcd1bf3c21a8a6a
Aaron Danielpour <amd144@case.edu>
2015-03-05 14:38:53 -0500

    Changed some of the syntax of comments

--- Assets/WorldNav.cs
+++ Assets/WorldNav.cs
@@ -2,8 +2,8 @@
 using System.Collections;
 
 public class WorldNav : MonoBehaviour {
-	//flag to check if the user has tapped / clicked. 
-	//Set to true on click. Reset to false on reaching destination
+ 
+	//Checks whether the button has been clicked.
 	private bool flag = false;
 	//destination point
 	private Vector3 endPoint;
@@ -25,28 +25,28 @@ public class WorldNav : MonoBehaviour {
 		{
 			//declare a variable of RaycastHit struct
 			RaycastHit hit;
-			//Create a Ray on the tapped / clicked position
+			//Create a Ray on the clicked position
 			Ray ray;
 
 			ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
 			//Check if the ray hits any collider
-			if(Physics.Raycast(ray,out hit))
-			{
+			if(Physics.Raycast(ray,out hit)) {
 				//set a flag to indicate to move the gameobject
 				flag = true;
-				//save the click / tap position
+				//save the clicked position
 				endPoint = hit.point;
-				//as we do not want to change the y axis value based on touch position, reset it to original y axis value
+				//as we do not want to change the y axis value based on click position, reset it to original y axis value
 				endPoint.y = yAxis;
 				Debug.Log(endPoint);
 			}
 			
 		}
-		//check if the flag for movement is true and the current gameobject position is not same as the clicked / tapped position
-		if(flag && !Mathf.Approximately(gameObject.transform.position.magnitude, endPoint.magnitude)){ //&& !(V3Equal(transform.position, endPoint))){
+		//check if the flag for movement is true and the current gameobject position is not same as the clicked position
+		if(flag && !Mathf.Approximately(gameObject.transform.position.magnitude, endPoint.magnitude)){
 			//move the gameobject to the desired position
 			gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, endPoint, 1/(duration*(Vector3.Distance(gameObject.transform.position, endPoint))));
+			//make sure the object is not bumping into the walls.
 		}
 		//set the movement indicator flag to false if the endPoint and current gameobject position are equal
 		else if(flag && Mathf.Approximately(gameObject.transform.position.magnitude, endPoint.magnitude)) {
