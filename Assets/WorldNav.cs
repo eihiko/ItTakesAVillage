831cf65289ed585590ec017edb2ff99f6f33c43b
Aaron Danielpour <amd144@case.edu>
2015-03-24 14:52:08 -0400

    WorldNavigation

--- Assets/WorldNav.cs
+++ Assets/WorldNav.cs
@@ -6,8 +6,6 @@ public class WorldNav : MonoBehaviour {
 	public GameObject building;
 
 	Ray ray1;
-	RaycastHit hit1;
-
 	//Checks whether the button has been clicked.
 	private bool flag1 = false;
 	//Destination point
@@ -37,14 +35,14 @@ public class WorldNav : MonoBehaviour {
 	// Update is called once per frame
 	void Update() {
 
-		if (Input.GetMouseButtonDown (0)) {
+		//if (Input.GetMouseButtonDown (0)) {
 
-			ray1 = Camera.main.ScreenPointToRay(Input.mousePosition);
+		//	ray1 = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-			if(Physics.Raycast(ray1,out building.collider)) {
-				building.collider.renderer.material.color = Color.red;
-			}
-		}
+		//	if(Physics.Raycast(ray1,out building.collider)) {
+		//		building.collider.renderer.material.color = Color.red;
+		//	}
+		//}
 
 		//check if the screen is right-clicked   
 		if(Input.GetMouseButton(1))
