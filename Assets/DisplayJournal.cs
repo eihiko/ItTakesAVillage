8f7850886d7ccb4a9ab30dc5bc40f216674a5ec9
Lucy K Tan <lucyktan@gmail.com>
2015-03-24 14:56:21 -0400

    Change button inputs; display journal

---
+++ Assets/DisplayJournal.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayJournal : MonoBehaviour {
+	
+	StringReader reader;
+
+	public void displayJournal() {
+				TextAsset journal = (TextAsset)Resources.Load ("input.ext", typeof(TextAsset));
+				reader = new StringReader (input.text);
+				string txt = reader.ReadLine ();
+				Debug.LogType (txt);
+		}
+
+}
