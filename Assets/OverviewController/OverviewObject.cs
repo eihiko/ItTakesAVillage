ae13f3ed0aed951d17bc7f4a045f11885c6e1116
Benjamin Poreh <bnp26@case.edu>
2015-03-31 15:32:16 -0400

    adding overviewcontroller assets

---
+++ Assets/OverviewController/OverviewObject.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class OverviewObject : MonoBehaviour {
+
+	public bool isSelected = false;
+	public Material ObjectOverviewMat;
+	public ObjectInfoObject ObjectInfoCanvas;
+
+	float COLOR_DIFF = 0.5F;
+	Color specColor, selected, deselected;
+	OverviewObjectController OOC = new OverviewObjectController();
+
+
+	void Start () {
+		specColor = this.renderer.material.GetColor ("_SpecColor");
+		selected = new Color (specColor.a - COLOR_DIFF, specColor.g - COLOR_DIFF, specColor.b - COLOR_DIFF);
+		deselected = new Color (specColor.a + COLOR_DIFF, specColor.g + COLOR_DIFF, specColor.b + COLOR_DIFF);
+		ObjectInfoCanvas.gameObject.SetActive (false);
+	}
+
+	void OnMouseDown() {
+		Debug.Log ("mouse clicked on object");
+		if (isSelected == false) {
+			Activation (true);
+		} else {
+			Activation (false);
+		}
+	}
+
+	public void Activation (bool val) {
+		if (val) {
+			OOC.ActivateObject (this);
+		} else {
+			OOC.DeactivateObject (this);
+		}
+	}
+
+	public void setSelected() {
+		isSelected = true;
+		this.renderer.material.SetColor("_SpecColor", selected);
+		// set the object info name as the name of the object
+		//ex: if the object is a house the name will be set to House
+		ObjectInfoCanvas.gameObject.SetActive (true);
+	}
+
+	public void setDeselected() {
+		isSelected = false;
+		this.renderer.material.SetColor ("_SpecColor", deselected);
+		ObjectInfoCanvas.gameObject.SetActive (false);
+	}
+
+	void update(){
+		if (isSelected) {
+			setSelected ();
+		} else {
+			setDeselected (); 
+		}
+	}
+
+}
---
+++ Assets/OverviewController/OverviewObjectController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OverviewObjectController {
+
+	public OverviewObject activeObject = null;
+	// Use this for initialization
+
+	public void ActivateObject(OverviewObject obj) {
+		if (activeObject != null)
+			activeObject.setDeselected ();
+
+		activeObject = obj;
+		activeObject.setSelected ();
+	}
+
+	public void DeactivateObject(OverviewObject obj) {
+		activeObject.setDeselected ();
+		activeObject = null;
+	}
+}
