d4303085638b89c539cc99a68c21b9b51a172d63
Eihiko <oeihiko@gmail.com>
2015-04-06 15:28:58 -0400

    Register scenes as levels

---
+++ Assets/SceneControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneControl : MonoBehaviour {
+
+	public 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		
+		if(Input.GetKeyUp (KeyCode.I)){
+			Application.LoadLevel ("LifeTasks");
+		}
+		if(Input.GetKeyUp (KeyCode.P)){
+			Application.LoadLevel ("Placement");
+		}
+		if(Input.GetKeyUp (KeyCode.O)){
+			Application.LoadLevel ("Overworld");
+		}
+	
+	}
+}
