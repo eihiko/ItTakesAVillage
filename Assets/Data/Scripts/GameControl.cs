61c9bb7029a198cf09c8d689d01ee402fc8b6e54
jrg94 <jrg170@case.edu>
2015-04-16 15:13:14 -0400

    Revert "Added a title scene with crude multiple save support"
    This reverts commit 7be78f28055fe2596b8a63806b77a8167549ccb0.

--- Assets/Data/Scripts/GameControl.cs
+++ Assets/Data/Scripts/GameControl.cs
@@ -15,9 +15,6 @@ public class GameControl : MonoBehaviour {
 
 	public static GameControl control;
 
-	// String to which data will be saved and loaded //
-	private string save_name;
-
 	/**
 	 * Put variables that you would want to persist 
 	 * through scenes here: experience, health,
@@ -49,16 +46,8 @@ public class GameControl : MonoBehaviour {
 	private int health;
 	private int experience;
 	private string label;
-
-	public void Start() {
-		save_name = "playerinfo";
-	}
-
-	/**
-	 * Sets the save name and loads that save
-	 */
-	public void SetSave(string name) {
-		save_name = name;
+	
+	public void Start(){
 		Load ();
 	}
 
@@ -292,7 +281,7 @@ public class GameControl : MonoBehaviour {
 	 */
 	public void Save() {
 		BinaryFormatter bf = new BinaryFormatter();
-		FileStream file = File.Create(Application.persistentDataPath + "/" + save_name + ".dat");
+		FileStream file = File.Create(Application.persistentDataPath + "/playerinfo.dat");
 
 		PlayerData data = new PlayerData();
 		// Insert data from controller to data object
@@ -324,9 +313,9 @@ public class GameControl : MonoBehaviour {
 	 * on some integer input
 	 */
 	public void Load() {
-		if (File.Exists (Application.persistentDataPath + "/" + save_name + ".dat")) {
+		if (File.Exists (Application.persistentDataPath + "/playerinfo.dat")) {
 			BinaryFormatter bf = new BinaryFormatter();
-			FileStream file = File.Open(Application.persistentDataPath + "/" + save_name + ".dat", FileMode.Open);
+			FileStream file = File.Open(Application.persistentDataPath + "/playerinfo.dat", FileMode.Open);
 			PlayerData data = (PlayerData)bf.Deserialize(file);
 			file.Close ();
 
@@ -355,8 +344,8 @@ public class GameControl : MonoBehaviour {
 	}
 	
 	public void Reset() {
-		if (File.Exists (Application.persistentDataPath + "/" + save_name + ".dat")) {
-			File.Delete (Application.persistentDataPath + "/" + save_name + ".dat");
+		if (File.Exists (Application.persistentDataPath + "/playerinfo.dat")) {
+			File.Delete (Application.persistentDataPath + "/playerinfo.dat");
 		}
 		this.SetStone (0);
 		this.SetCoin (0);
