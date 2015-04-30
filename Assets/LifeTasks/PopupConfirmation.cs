69d3b5f7585afa9c6794918fb11efcf3148e5e01
Lucy K Tan <lucyktan@gmail.com>
2015-04-29 21:20:00 -0400

    Add popup confirmation (but does not show text?!)

---
+++ Assets/LifeTasks/PopupConfirmation.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEngine.UI;
+using System;
+
+public class PopupConfirmation : MonoBehaviour {
+
+	public Text window;
+	public Button b;
+
+	// Use this for initialization
+	void Start () {
+		gameObject.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		generateMessage (b);
+	}
+	
+	public void generateMessage(Button b) {
+		window.text = "Are you sure you completed this task?";
+		gameObject.SetActive (true);
+	}
+
+	public void closeWindow(Button b) {
+		gameObject.SetActive (false);
+	} 
+}
